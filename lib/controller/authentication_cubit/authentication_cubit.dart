408cedbd6be3ada69b851e883f1a05060f12b28a
Halawany1 <mdnjdhdhd44@gmail.com>
2024-06-27 00:11:58 +0300

    intail commit

---
+++ lib/controller/authentication_cubit/authentication_cubit.dart
@@ -0,0 +1,10 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'authentication_state.dart';
+
+class AuthenticationCubit extends Cubit<AuthenticationState> {
+  AuthenticationCubit() : super(AuthenticationInitial());
+
+
+}
---
+++ lib/core/constant/image_constant.dart
@@ -0,0 +1,4 @@
+class Images{
+
+  static const String signInLogo = "assets/images/signin.jpg";
+}
